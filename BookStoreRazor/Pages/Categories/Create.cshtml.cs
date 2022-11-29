b2ea489f87745f964452e8aeaaeee924d8efd604
Yogesh Koli <yogeshkoli@hotmail.com>
2022-11-29 14:04:42 +0530

    Adding custom modelstate error & listing validation summary on razor page

--- BookStoreRazor/Pages/Categories/Create.cshtml.cs
+++ BookStoreRazor/Pages/Categories/Create.cshtml.cs
@@ -30,6 +30,12 @@ namespace BookStoreRazor.Pages.Categories
 
         public async Task<IActionResult> OnPost()
         {
+
+            if(Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "The DisplayOrder cannot exactly match the Name.");
+            }
+
             if (ModelState.IsValid)
             {
                 await _storeContext.Category.AddAsync(Category);
